ea3e740369fc86449329b88aa0944a12c0b368ac
Apathy789 <100036547+Apathy789@users.noreply.github.com>
2022-03-04 01:30:05 +0800

    Update ql-update-dependence.sh

--- ql-update-dependence.sh
+++ ql-update-dependence.sh
@@ -7,4 +7,26 @@ npm install tunnel
 npm install ws
 npm install jsdom
 npm install png-js
+pnpm i#!/bin/bash
+
+npm install crypto-js
+
+npm install got
+
+npm install request
+
+npm install tough-cookie
+
+npm install tunnel
+
+npm install ws
+
+npm install jsdom
+
+npm install png-js
+
 pnpm i
+
+pip3 install selenium
+
+pip install selenium
